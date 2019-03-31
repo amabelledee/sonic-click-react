7ba3bc1891731a0ce9e59ade6dbb0b7c52b930f0
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-03-31 16:21:23 -0700

    Updates

---
+++ precache-manifest.297a7ca8502c116d325f52b9b10a3950.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "313a39a582ae1a3bba06",
+    "url": "/sonic-click-react/static/css/main.21bba3bb.chunk.css"
+  },
+  {
+    "revision": "313a39a582ae1a3bba06",
+    "url": "/sonic-click-react/static/js/main.3096a3fe.chunk.js"
+  },
+  {
+    "revision": "145b6552b6ddca75f6ce",
+    "url": "/sonic-click-react/static/js/runtime~main.6f0082e1.js"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/sonic-click-react/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "bb233b9c4adeab7a4730fd193013cbcf",
+    "url": "/sonic-click-react/static/media/doc11.bb233b9c.jpg"
+  },
+  {
+    "revision": "2ed2db0dd07e00ffd502e5a29f84f64a",
+    "url": "/sonic-click-react/static/media/cyberman.2ed2db0d.jpg"
+  },
+  {
+    "revision": "92dd243a1410bd7a016f3e70c3328eae",
+    "url": "/sonic-click-react/static/media/amy.92dd243a.jpg"
+  },
+  {
+    "revision": "220c6ee3aa4eb22b6274a5622fe55e42",
+    "url": "/sonic-click-react/static/media/river.220c6ee3.jpg"
+  },
+  {
+    "revision": "4a92e8737b170b783b4c702a7793633f",
+    "url": "/sonic-click-react/index.html"
+  }
+];
